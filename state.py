88f25d715c368e39dfcdfe81db5f8c892db625bd
Vladislav Radovic <vradovic522@gmail.com>
2022-05-31 22:26:20 +0200

    Stretch out board so it is easier to read it

--- state.py
+++ state.py
@@ -226,17 +226,17 @@ class State(object):
     # Ispisivanje table
     def __str__(self):
         return f"""
-        {self._board[0]}-----{self._board[1]}-----{self._board[2]}\t0, 1, 2
-        |     |     |
-        | {self._board[3]}---{self._board[4]}---{self._board[5]} |\t3, 4, 5
-        | |   |   | |
-        | | {self._board[6]}-{self._board[7]}-{self._board[8]} | |\t6, 7, 8
-        | | |   | | |
-        {self._board[9]}-{self._board[10]}-{self._board[11]}   {self._board[12]}-{self._board[13]}-{self._board[14]}\t9, 10, 11, 12, 13, 14
-        | | |   | | |
-        | | {self._board[15]}-{self._board[16]}-{self._board[17]} | |\t15, 16, 17
-        | |   |   | |
-        | {self._board[18]}---{self._board[19]}---{self._board[20]} |\t18, 19, 20
-        |     |     |
-        {self._board[21]}-----{self._board[22]}-----{self._board[23]}\t21, 22, 23
+        {self._board[0]}----------{self._board[1]}----------{self._board[2]}\t0, 1, 2
+        |          |          |
+        |  {self._board[3]}-------{self._board[4]}-------{self._board[5]}  |\t3, 4, 5
+        |  |       |       |  |
+        |  |  {self._board[6]}----{self._board[7]}----{self._board[8]}  |  |\t6, 7, 8
+        |  |  |         |  |  |
+        {self._board[9]}--{self._board[10]}--{self._board[11]}         {self._board[12]}--{self._board[13]}--{self._board[14]}\t9, 10, 11, 12, 13, 14
+        |  |  |         |  |  |
+        |  |  {self._board[15]}----{self._board[16]}----{self._board[17]}  |  |\t15, 16, 17
+        |  |       |       |  |
+        |  {self._board[18]}-------{self._board[19]}-------{self._board[20]}  |\t18, 19, 20
+        |          |          |
+        {self._board[21]}----------{self._board[22]}----------{self._board[23]}\t21, 22, 23
         """
